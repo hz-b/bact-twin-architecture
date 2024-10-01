d7c877ee9bba0f5969e5316998d062d1b632e2b8
Pierre Schnizer <pierre.schnizer@helmholtz-berlin.de>
2024-10-01 18:33:43 +0200

    [TASK] removed dedicated identifers for lattice element, device, or lattice position

--- src/bact_twin_architecture/interfaces/__init__.py
+++ src/bact_twin_architecture/interfaces/__init__.py
@@ -2,7 +2,6 @@ __all__ = [
     "command_state_space_transformer",
     "family_tree",
     "id_property_transformer",
-    "identifier",
     "measurement_execution_engine",
     "state_conversion",
 ]
