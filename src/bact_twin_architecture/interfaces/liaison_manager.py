48ec4ef32294981882a10a314407bbc8573abe59
Pierre Schnizer <pierre.schnizer@helmholtz-berlin.de>
2025-01-29 17:46:01 +0100

    [TASK] mapping from a one to many

--- src/bact_twin_architecture/interfaces/liaison_manager.py
+++ src/bact_twin_architecture/interfaces/liaison_manager.py
@@ -1,14 +1,20 @@
 from abc import ABCMeta, abstractmethod
+from typing import Sequence
+
 from ..data_model.identifiers import LatticeElementPropertyID, DevicePropertyID
 
 
 class LiaisonManagerBase(metaclass=ABCMeta):
     """transforms pairs of (id, property)
+
+    Warning:
+        it returns a sequence of device / properties
+        More than one device can be necessary to be updated
     """
     @abstractmethod
-    def forward(self, id_: LatticeElementPropertyID) -> DevicePropertyID:
+    def forward(self, id_: LatticeElementPropertyID) -> Sequence[DevicePropertyID]:
         raise NotImplementedError("use derived class instead")
 
     @abstractmethod
-    def inverse(self, id_: DevicePropertyID) -> LatticeElementPropertyID:
+    def inverse(self, id_: DevicePropertyID) -> Sequence[LatticeElementPropertyID]:
         raise NotImplementedError("use derived class instead")
