8426857509541a165e624a30856f8ee3c352ac58
Waheed <wsulimankhail@gmail.com>
2025-12-08 14:46:08 +0100

    [TASK] soleil yp drafted

--- src/bact_twin_architecture/bl/soleil_yellow_pages.py
+++ src/bact_twin_architecture/bl/soleil_yellow_pages.py
@@ -1,7 +1,7 @@
-from enum import Enum
-from typing import Sequence, Union
 import json
+from enum import Enum
 from pathlib import Path
+from typing import Sequence, Union
 
 from ..interfaces.family_tree import FamilyTree
 
@@ -11,6 +11,8 @@ class FamilyName(Enum):
     sextupoles = "sextupoles"
     bends = "bends"
     multipoles = "multipoles"
+    horizontal_steerers = "horizontal_steerers"
+    vertical_steerers = "vertical_steerers"
 
 
 class YellowPages(FamilyTree):
@@ -32,6 +34,12 @@ class YellowPages(FamilyTree):
     def multipole_names(self) -> Sequence[str]:
         return self.get("multipoles")
 
+    def horizontal_steerer_names(self) -> Sequence[str]:
+        return self.get("horizontal_steerers")
+
+    def vertical_steerer_names(self) -> Sequence[str]:
+        return self.get("vertical_steerers")
+
 
 def soleil_yellow_pages() -> YellowPages:
     """
@@ -45,15 +53,19 @@ def soleil_yellow_pages() -> YellowPages:
     elements = json.loads(data_file.read_text())
 
     quadrupoles = [e["name"] for e in elements if e["type"] == "Quadrupole"]
-    sextupoles  = [e["name"] for e in elements if e["type"] == "Sextupole"]
-    bends       = [e["name"] for e in elements if e["type"] == "Bend"]
-    multipoles  = [e["name"] for e in elements if e["type"] == "Multipole"]
+    sextupoles = [e["name"] for e in elements if e["type"] == "Sextupole"]
+    bends = [e["name"] for e in elements if e["type"] == "Bend"]
+    multipoles = [e["name"] for e in elements if e["type"] == "Multipole"]
+    horizontal_steerers = [e["name"] for e in elements if e["type"] == "None"]
+    vertical_steerers = [e["name"] for e in elements if e["type"] == "None"]
 
     d = dict(
         quadrupoles=quadrupoles,
         sextupoles=sextupoles,
         bends=bends,
         multipoles=multipoles,
+        horizontal_steerers=horizontal_steerers,
+        vertical_steerers=vertical_steerers,
     )
 
     return YellowPages(d)
